4ee643a52d85874ab928ad33080c869165a7c741
maximkaG <maxga.2000@mail.ru>
2022-03-25 00:20:07 +0700

    first commit

---
+++ gma_sp2.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+short Abs(short a) {
+	return a < 0 ? -1 * a : a;
+}
+
+int main() {
+	short arr[10] = {-11000, -40, 56, 62, -74, -70, 199, -98, -19, 82};
+	short answer = 0;
+	for (int i = 0; i < 10; i++) {
+		answer += Abs(arr[i]);
+	}
+	printf("%d\n", answer);
+	return 0;
+}
